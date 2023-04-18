5a6780fe4afb2b7e76425a4ae33f824983166d3e
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-18 17:02:22 -0300

    Exercicio Resolvido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int valor;
+
+Console.Write("Digite um número: ");
+valor = Int32.Parse(Console.ReadLine()!);
+
+bool par = valor%2 == 0;
+
+if (!par)
+{
+    Console.WriteLine($"\n{valor} é Impar");
+}
+
+else if (par)
+{
+    Console.WriteLine($"\n{valor} é Par");
+}
+
+Environment.Exit(-1); //Finaliza o Programa.
